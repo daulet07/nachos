cc1ceb6109631fe876c1bb4a824681750be08181
adhatarao <mail.ssupriya@gmail.com>
2016-01-24 21:40:09 +0100

    testing part 4

--- code/test/multiprocess.c
+++ code/test/multiprocess.c
@@ -1,7 +1,11 @@
 #include "syscall.h"
 int main()
 {
-	if (ForkExec("userpages0") == -1)
+	if (ForkExec("step4") == -1)
+		PutString("No Memory to fork the process\n");
+	if (ForkExec("p1") == -1)
+		PutString("No Memory to fork the process\n");
+	/*if (ForkExec("userpages0") == -1)
 		PutString("No Memory to fork the process\n");
 	if (ForkExec("userpages1") == -1)
 		PutString("No Memory to fork the process\n");
@@ -15,12 +19,8 @@ int main()
 		PutString("No Memory to fork the process\n");
 	if (ForkExec("userpages0") == -1)
 		PutString("No Memory to fork the process\n");
-	if (ForkExec("userpages1") == -1)
-		PutString("No Memory to fork the process\n");
-	if (ForkExec("userpages0") == -1)
-		PutString("No Memory to fork the process\n");
-//	if (ForkExec("userpages1") == -1)
-		//PutString("No Memory to fork the process\n");
+	/ *if (ForkExec("putchar") == -1)
+		PutString("No Memory to fork the process\n");*/
 	PutString("End main\n");
 	return 0;
 }
