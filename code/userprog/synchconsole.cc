f1f9d548800c2bf75427df8638b27a07795f2f15
Daulet Bari <daulet.bari@gmail.com>
2016-01-27 11:39:24 +0100

    cleaned synchconsole.cc

--- code/userprog/synchconsole.cc
+++ code/userprog/synchconsole.cc
@@ -7,12 +7,6 @@
 static Semaphore *readAvail;
 static Semaphore *writeDone;
 
-//static Semaphore *semPut;
-//static Semaphore *semGet;
-
-//static Semaphore *semPutString;
-//static Semaphore *semGetString;
-
 static void ReadAvail(int arg) {readAvail->V();}
 static void WriteDone(int arg) {writeDone->V();}
 
@@ -21,13 +15,8 @@ SynchConsole::SynchConsole(char *readFile, char *writeFile)
 	readAvail = new Semaphore("read avail", 0);
 	writeDone = new Semaphore("write done", 0);
 	console = new Console(readFile, writeFile, ReadAvail, WriteDone, 0);
-//	semPut = new Semaphore("sem put", 1);
-//	semGet = new Semaphore("sem get", 1);
 	lockPut = new Lock("lock put");
 	lockGet = new Lock("lock put");
-
-//	semPutString = new Semaphore("sem put string", 1);
-//	semGetString = new Semaphore("sem get string", 1);
 }
 
 SynchConsole::~SynchConsole()
@@ -37,63 +26,52 @@ SynchConsole::~SynchConsole()
 	delete readAvail;
 	delete lockPut;
 	delete lockGet;
-//	delete semPut;
-//	delete semGet;
-
-//	delete semPutString;
-//	delete semGetString;
 }
 
 void SynchConsole::SynchPutChar(const char ch)
 {
-//	semPut->P();
 	lockPut->Acquire();
 	console->PutChar(ch);
 	writeDone->P();
 	lockPut->Release();
-//	semPut->V();
 }
 
 char SynchConsole::SynchGetChar()
 {
-//	semGet->P();
 	lockGet->Acquire();
 	readAvail->P();
 
 	char ch;
 	ch = console->GetChar();
 	lockGet->Release();
-//	semGet->V();
 	return ch;
 }
 
 void SynchConsole::SynchPutString(const char s[], int length)
 {
-	for (int i = 0; i < length; i ++, s ++)
+	for (int i = 0; i < length; i++, s++)
 		SynchPutChar(*s);
 }
 
 void SynchConsole::SynchPutString(const char s[])
 {
-//	semPutString->P();
 	while (*s != '\0')
 	{
 		SynchPutChar(*s);
-		s ++;
+		s++;
 	}
-//	semPutString->V();
 }
 
 int SynchConsole::SynchGetString(char *s, int n)
 {
-//	semGetString->P();
 	int i = 0;
-	do{
+	do {
 		s[i] = SynchGetChar();
 		i ++;
-	}while(i < n && s[i-1] != '\0' && s[i-1] != '\n');
+	} while (i < n && s[i-1] != '\0' && s[i-1] != '\n');
 	return i;
-	for (i = 0; i < n; i ++)
+
+	for (i = 0; i < n; i++)
 	{
 		s[i] = SynchGetChar();
 		if (s[i] == '\n' || s[i] == EOF || s[i] == '\0')
@@ -102,7 +80,6 @@ int SynchConsole::SynchGetString(char *s, int n)
 			break;
 		}
 	}
-//	semGetString->V();
 
 	return i;
 }
@@ -112,7 +89,7 @@ void SynchConsole::SynchPutInt(int n)
 	char *buffer = new char[MAX_STRING_SIZE];
 	snprintf(buffer, MAX_STRING_SIZE, "%d", n);
 	SynchPutString(buffer);
-	delete []buffer;
+	delete[] buffer;
 }
 
 void SynchConsole::SynchGetInt(int *n)
@@ -121,7 +98,7 @@ void SynchConsole::SynchGetInt(int *n)
 	SynchGetString(buffer, MAX_STRING_SIZE);
 	if (sscanf(buffer, "%i", n) != 1)
 		*n = 0;
-	delete [] buffer;
+	delete[] buffer;
 }
 
 #endif // CHANGED
