01569b6586c175e3ea0863345c74c544a916ddec
jeremie finiel <jeremiefiniel@gmail.com>
2016-01-27 11:04:46 +0100

    add prepare.sh script

---
+++ code/build/prepare.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo Format filesystem
+./nachos-step5 -f
+echo Copy shell
+./nachos-step5 -cp shell /shell
+
+./nachos-step5 -mkdir /IO
+./nachos-step5 -cp getchar /IO/getchar
+./nachos-step5 -cp putchar /IO/putchar
+./nachos-step5 -cp getstring /IO/getstring
+./nachos-step5 -cp putstring /IO/putstring
+./nachos-step5 -cp getint /IO/getint
+./nachos-step5 -cp putint /IO/putint
+
+./nachos-step5 -mkdir /vm
+./nachos-step5 -cp thread /vm/thread
+./nachos-step5 -cp sem /vm/sem
+./nachos-step5 -cp pc /vm/pc
+./nachos-step5 -cp multiprocess /vm/multiprocess
+./nachos-step5 -cp multithread /vm/multithreads
+
+./nachos-step5 -mkdir /filesys
+./nachos-step5 -cp filesys /filesys/filesys
+
+#for prog in `find -type f -executable`; do
+#	./nachos-step5 -cp $prog /bin${prog:1}
+#done
+
+#./nachos-step5 -cp shell /shell
+#./nachos-step5 -cp muli
+#./nachos-step5 -f
+#./nachos-step5 -f
